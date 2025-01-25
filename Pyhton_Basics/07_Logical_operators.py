f964293d5a2038f9f46c793eedcb56377ba6d76a
ANUJ KUMAR RAI <111315232+Anuj2402@users.noreply.github.com>
2025-01-25 00:10:36 +0000

    Logical Operators

---
+++ Pyhton_Basics/07_Logical_operators.py
@@ -0,0 +1,36 @@
+#Logical Operators -> Evaluate the expression and return True or False based on the condition (or , and, not)
+#                 -> or -> If any one of the condition is True, The result is True else False
+#                 -> and -> If all the condition is True, The result is True else False 
+#                 -> not -> If the condition is True, The result is False and vice versa
+
+#Exercises05-> Based on the temperature and weather condition, print whether the weather is suitable for playing cricket
+'''
+Temperature = 30;
+is_raining = True;
+if Temperature > 35 or temp<0 or is_raining:
+    print("The weather is not suitable for playing cricket");
+else:
+    print("The weather is suitable for playing cricket");
+'''
+#Exercises06-> Based on the age of the user, print whether the user is eligible for voting
+'''
+age = 15;
+if age >= 18 and age <= 100:
+    print("You are eligible for voting");   
+else:
+    print("You are not eligible for voting");
+'''
+#And operator Example
+'''
+if 10 > 5 and 10 < 20:
+    print("Both the conditions are True");  
+else:
+    print("Both the conditions are not True");
+'''
+#Not operator Example
+'''
+if not 10 > 5:
+    print("The condition is True");
+else:
+    print("The condition is False");
+'''
